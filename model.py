e08c97a19d2fd26b70e9db3332d01c69b66df6ff
Bertan Dogancay <111835151+BertanDogancay@users.noreply.github.com>
2023-04-13 06:51:51 -0400

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from sklearn.metrics import accuracy_score, recall_score
 import pickle
 
 
-def feature_extract(df):
+def feature_extract(df, window_size=50):
     # Mean of acceleration
     df['mean_x'] = df['Linear Acceleration x (m/s^2)'].rolling(window=window_size).mean()
     df['mean_y'] = df['Linear Acceleration y (m/s^2)'].rolling(window=window_size).mean()
@@ -35,56 +35,53 @@ def feature_extract(df):
     df['y_z_corr'] = df['Linear Acceleration y (m/s^2)'].rolling(window=window_size).corr(df['Linear Acceleration z (m/s^2)'])
 
     df.dropna(inplace=True)
-    
 
-# Use an SMA on the training data
+def create_model():
+    # Use an SMA on the training data
+    df_train = pd.read_csv('data/train_data.csv')
+    df_test = pd.read_csv('data/test_data.csv')
+    window_size = 31
 
-df_train = pd.read_csv('data\\train_data.csv')
-df_test = pd.read_csv('data\\test_data.csv')
-window_size = 31
+    sma = df_train.iloc[:,0:-1].rolling(window_size).mean()
+    sma['label'] = df_train.iloc[:,-1]
+    sma = sma.dropna()
 
-sma = df_train.iloc[:,0:-1].rolling(window_size).mean()
-sma['label'] = df_train.iloc[:,-1]
-sma = sma.dropna()
+    # Do feature extraction on train and test data
+    window_size = 50
 
+    feature_extract(sma)
+    feature_extract(df_test)
 
-# Do feature extraction on train and test data
-window_size = 50
+    # get rid of raw data (time and accels)
+    sma = sma.iloc[:,5:]
+    df_test = df_test.iloc[:,5:]
 
-feature_extract(sma)
-feature_extract(df_test)
+    # Normalize the data
+    # Separate input features from target variable
+    X_train = sma.drop('label', axis=1)
+    X_test = df_test.drop('label', axis=1)
 
-# get rid of raw data (time and accels)
-sma = sma.iloc[:,5:]
-df_test = df_test.iloc[:,5:]
+    # Normalize input features and store them
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
 
-# Normalize the data
-# Separate input features from target variable
-X_train = sma.drop('label', axis=1)
-X_test = df_test.drop('label', axis=1)
+    # Separate the target variable from the normalized input features
+    y_train = sma['label']
+    y_test = df_test['label']
 
-# Normalize input features and store them
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.fit_transform(X_test)
+    # Train a logistic regression model
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
 
-# Separate the target variable from the normalized input features
-y_train = sma['label']
-y_test = df_test['label']
+    # Evaluate the model on the test set
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
 
+    recall = recall_score(y_test, y_pred) 
+    print('recall is: ', recall) 
 
-# Train a logistic regression model
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
-
-# Evaluate the model on the test set
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
-
-recall = recall_score(y_test, y_pred) 
-print('recall is: ', recall) 
-
-# save the model to a file
-filename = 'logistic_regression_model.sav'
-pickle.dump(model, open(filename, 'wb'))
+    # save the model to a file
+    filename = 'logistic_regression_model.sav'
+    pickle.dump(model, open(filename, 'wb'))
